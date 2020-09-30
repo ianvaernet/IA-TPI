b146ecb71f67cd2c0e24a826e45129b2f7003046
Ian Vaernet <ianalexisnet@hotmail.com>
2020-09-30 03:05:15 -0300

    Refactorization. Classes added. Avoid re-classify training data on every plot update.

--- table.js
+++ table.js
@@ -1,41 +1,46 @@
-const tableBodyId = 'table-body';
-const KNNTableBodyId = 'knn-table-body';
+const datasetTableBodyId = 'table-body';
+const knnTableBodyId = 'knn-table-body';
 
-function removeAllChildNodes(parent) {
-  while (parent.firstChild) {
-    parent.removeChild(parent.firstChild);
+class Tables {
+  constructor() {
+    this.datasetTableBody = document.getElementById(datasetTableBodyId);
+    this.knnTableBody = document.getElementById(knnTableBodyId);
   }
-}
 
-function addTableData(tr, value) {
-  const td = document.createElement('td');
-  td.innerText = value;
-  tr.appendChild(td);
-}
+  removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+      parent.removeChild(parent.firstChild);
+    }
+  }
 
-function updateTable(data) {
-  const tableBody = document.getElementById(tableBodyId);
-  removeAllChildNodes(tableBody);
-  data.forEach((d) => {
-    const tr = document.createElement('tr');
-    addTableData(tr, d.x);
-    addTableData(tr, d.y);
-    addTableData(tr, d.label);
-    tableBody.appendChild(tr);
-  });
-}
+  addTableData(tr, value) {
+    const td = document.createElement('td');
+    td.innerText = value;
+    tr.appendChild(td);
+  }
 
-function updateKNNTable(orderedTrainingDataWithDistances, k) {
-  const KNNTableBody = document.getElementById(KNNTableBodyId);
-  removeAllChildNodes(KNNTableBody);
-  const max = orderedTrainingDataWithDistances.length < k ? orderedTrainingDataWithDistances.length : k;
-  for (let i = 0; i < max; i++) {
-    const data = orderedTrainingDataWithDistances[i];
-    const tr = document.createElement('tr');
-    addTableData(tr, data.x.toFixed(4));
-    addTableData(tr, data.y.toFixed(4));
-    addTableData(tr, data.label);
-    addTableData(tr, data.distance.toFixed(4));
-    KNNTableBody.appendChild(tr);
+  updateDatasetTable(data) {
+    this.removeAllChildNodes(this.datasetTableBody);
+    data.forEach((d) => {
+      const tr = document.createElement('tr');
+      this.addTableData(tr, d.x);
+      this.addTableData(tr, d.y);
+      this.addTableData(tr, d.label);
+      this.datasetTableBody.appendChild(tr);
+    });
+  }
+
+  updateKnnTable(orderedTrainingDataWithDistances, k) {
+    this.removeAllChildNodes(this.knnTableBody);
+    const max = orderedTrainingDataWithDistances.length < k ? orderedTrainingDataWithDistances.length : k;
+    for (let i = 0; i < max; i++) {
+      const data = orderedTrainingDataWithDistances[i];
+      const tr = document.createElement('tr');
+      this.addTableData(tr, data.x.toFixed(4));
+      this.addTableData(tr, data.y.toFixed(4));
+      this.addTableData(tr, data.label);
+      this.addTableData(tr, data.distance.toFixed(4));
+      this.knnTableBody.appendChild(tr);
+    }
   }
 }
