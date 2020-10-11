54aa028e0170b248275a7480380d260d45f034e1
Leon <leon.isla.22@gmail.com>
2020-10-11 19:18:17 -0300

    small tweack getColor method

--- labels.js
+++ labels.js
@@ -23,13 +23,24 @@ class Labels {
 
   setLabels(labels) {
     this.labels = labels;
-    this.labelColor = { NaN: '#000000' };
+    this.labelColor = { NaN: '#00000000' };
     labels.forEach((label, i) => {
       if (label !== 'NaN') this.labelColor[label] = this.colors[i % this.colors.length];
     });
   }
 
-  getColor(label) {
-    return this.labelColor[label];
+  getColor(label, alpha) {
+    if (alpha) {
+      const color = this.labelColor[label];
+      const colorPart = color.slice(0, 7);
+      const alphaPart = color.slice(7, 9);
+      if (alphaPart.length > 0) {
+        return alphaPart === "|1" ? colorPart : color;
+      } else {
+        return color + alpha;
+      }
+    } else {
+      return this.labelColor[label];
+    }
   }
 }
